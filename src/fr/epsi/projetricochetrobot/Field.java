4a5f9b5bedb6407a91f2704ea15010afc051741e
florian <f.baudry@hotmail.fr>
2014-03-05 16:54:30 +0100

    - Création de la classe Field

---
+++ src/fr/epsi/projetricochetrobot/Field.java
@@ -0,0 +1,37 @@
+package fr.epsi.projetricochetrobot;
+
+public class Field {
+	
+	int nbRound;
+	boolean isFinished;
+	
+	
+	
+	
+	public void incrementNbRound()
+	{
+		nbRound++;
+	}
+	
+	public void setNbRound(int nbRound)
+	{
+		this.nbRound = nbRound;
+	}
+	
+	public int getNbRound()
+	{
+		return this.nbRound;
+	}
+	
+	public void isFinished(boolean isFinished)
+	{
+		this.isFinished = isFinished;
+	}
+	
+	public boolean getIsFinished()
+	{
+		return this.isFinished;
+	}
+	
+
+}
